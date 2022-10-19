9dfad1f70b215c7333d35fc9a131795907154cee
NoridaGuacheta <115855459+NoridaGuacheta@users.noreply.github.com>
2022-10-19 09:23:49 -0500

    Update ReservationCRUDRepository.java

--- src/main/java/com/misiontic/usergioarboleda/Ortesis/repositories/ReservationCRUDRepository.java
+++ src/main/java/com/misiontic/usergioarboleda/Ortesis/repositories/ReservationCRUDRepository.java
@@ -4,4 +4,11 @@ import com.misiontic.usergioarboleda.Ortesis.models.Reservation;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ReservationCRUDRepository extends CrudRepository<Reservation, Integer> {
+  
+   public List<Reservation> findAllByStatus(String status);
+    
+    public List<Reservation> findAllByStartDateAfterAndStartDateBefore(Date datoUno, Date datodos);
+    
+    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client) DESC")
+    public List<Object[]> countTotalReservationByClient();
 }
