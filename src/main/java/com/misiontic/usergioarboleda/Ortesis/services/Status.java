25a64f38352126143af60b8b38dd238eae977cea
NoridaGuacheta <115855459+NoridaGuacheta@users.noreply.github.com>
2022-10-19 09:41:46 -0500

    Add files via upload

---
+++ src/main/java/com/misiontic/usergioarboleda/Ortesis/services/Status.java
@@ -0,0 +1,29 @@
+package com.misiontic.usergioarboleda.Ortesis.services;
+
+public class Status {
+
+    private int completed;
+    private int cancelled;
+
+    public Status(int completed, int cancelled) {
+        this.completed = completed;
+        this.cancelled = cancelled;
+    }
+
+    public int getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(int completed) {
+        this.completed = completed;
+    }
+
+    public int getCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(int cancelled) {
+        this.cancelled = cancelled;
+    }
+
+}
