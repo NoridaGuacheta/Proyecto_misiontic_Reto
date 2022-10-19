b085cb0af8345f821676cbaa212a3c453dcab658
NoridaGuacheta <115855459+NoridaGuacheta@users.noreply.github.com>
2022-10-19 11:46:18 -0500

    Update ReservationRepository.java

--- src/main/java/com/misiontic/usergioarboleda/Ortesis/repositories/ReservationRepository.java
+++ src/main/java/com/misiontic/usergioarboleda/Ortesis/repositories/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.misiontic.usergioarboleda.Ortesis.repositories;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import com.misiontic.usergioarboleda.Ortesis.models.Client;
 import com.misiontic.usergioarboleda.Ortesis.models.Reservation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -35,16 +38,16 @@ public class ReservationRepository {
     // ============ Informe ============ //
     
     public List<Reservation> getReservationByStatus (String status){
-        return reservationCrudRepository.findAllByStatus(status);
+        return reservationCRUDRepository.findAllByStatus(status);
     }
     
     public List<Reservation> informePeriodoTiempoReservas (Date a, Date b){
-        return reservationCrudRepository.findAllByStartDateAfterAndStartDateBefore(a, b);
+        return reservationCRUDRepository.findAllByStartDateAfterAndStartDateBefore(a, b);
     }
     
     public List<CountClient> getTopClient(){
         List<CountClient> res = new ArrayList<>();
-        List<Object[]> report = reservationCrudRepository.countTotalReservationByClient();
+        List<Object[]> report = reservationCRUDRepository.countTotalReservationByClient();
         for(int i=0;i < report.size();i++){
             res.add(new CountClient((Long)report.get(i)[1], (Client) report.get(i)[0]));
         }
